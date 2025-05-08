d5b31cac43e4b6728e9281c730b06849fe628ea5
Matheus Mathias <mathias.matheus76@gmail.com>
2025-05-08 09:02:40 -0300

    add: html e campos de configuração

---
+++ edit_form.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+class block_course_gallery_edit_form extends block_edit_form {
+
+    protected function specific_definition($mform) {
+        // Seção para as configurações.
+        $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
+
+        $mform->addElement('text', 'config_gallery_title', get_string('add_title', 'block_course_gallery'));
+        $mform->setDefault('config_gallery_title', 'Cursos abertos do IFRN');
+        $mform->setType('config_gallery_title', PARAM_TEXT);
+    }
+}
